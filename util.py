2d509721d819cbbf03f54dcbf6b40cc9de7ac960
DiegoPaez <diripar8@gmail.com>
2023-11-09 16:34:54 -0300

    plot training history

--- util.py
+++ util.py
@@ -2,6 +2,8 @@ import yaml
 import pickle as pkl
 import os
 import re
+import matplotlib.pyplot as plt
+
 
 
 def natural_key(string):
@@ -65,3 +67,39 @@ def check_file_exists(file_path):
         return True
     else:
         return False
+    
+def plot_training_history(results_dict:dict, params:dict):
+    """ Plot the training history of a model.
+    
+    Args:
+        results_dict: (dict) dictionary with the training history.
+    """
+        
+    fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+    
+    total_epochs = len(results_dict["training_losses"])
+    epochs = range(1, total_epochs + 1)
+    eval_starts = params["max_epochs"] - params["epochs_to_eval"]
+    epochs_val = range(eval_starts+1, total_epochs+1)
+    
+    ax[0].plot(epochs, results_dict["training_losses"], 'b', label='Training loss')
+    ax[0].plot(epochs_val, results_dict["validation_losses"], 'r', label='Validation loss')
+    ax[0].set_title('Loss')
+    ax[0].set_xlabel('Epoch')
+    ax[0].set_ylabel('Loss')
+    ax[0].legend()
+    ax[0].grid(True)
+    
+
+    ax[1].plot(epochs, results_dict["training_accuracies"], 'b', label='Training Acc')
+    ax[1].plot(epochs_val, results_dict["validation_accuracies"], 'r', label='Validation Acc')
+    max_acc, index = max(results_dict["validation_accuracies"]), results_dict["validation_accuracies"].index(max(results_dict["validation_accuracies"]))
+    ax[1].plot(index+1, max_acc, 'go', label='Max Acc')
+    ax[1].text(index+1, max_acc+0.1, f'{max_acc:.2f}', fontsize=12)
+    ax[1].set_title('Accuracy')
+    ax[1].set_xlabel('Epoch')
+    ax[1].set_ylabel('Accuracy')
+    ax[1].legend()
+    ax[1].grid(True)
+    
+    plt.show()
