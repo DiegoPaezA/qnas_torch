fddf18a3534c902d1b31b35c92f89fb265388eba
DiegoPaez <diripar8@gmail.com>
2023-12-13 20:54:15 +0000

    run evolution launcher

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the evolution experiment
+echo "Running evolution experiment"
+
+python run_evolution.py \
+    --experiment_path my_exp_config2 \
+    --config_file 'config_files/config2.txt' \
+    --data_path cifar10_data \
+    --log_level INFO
