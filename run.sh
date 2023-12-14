594ccb194339755c368813a09fb167058fc52826
DiegoPaez <diripar8@gmail.com>
2023-12-14 20:09:54 +0000

    testing new configuration

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 echo "Running evolution experiment"
 
 python run_evolution.py \
-    --experiment_path my_exp_config2 \
+    --experiment_path my_exp_4 \
     --config_file 'config_files/config2.txt' \
     --data_path cifar10_data \
     --log_level INFO
