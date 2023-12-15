3e92e879ba528e19db4d5ef4d7c42caab1649b2e
DiegoPaez <diripar8@gmail.com>
2023-12-15 15:07:54 +0000

    prepare experiments

--- run_multi.sh
+++ run_multi.sh
@@ -38,3 +38,16 @@ for ((i=1; i<=5; i++)); do
         --data_path cifar10_data \
         --log_level INFO
 done
+
+# Run the evolution experiment multiple times with different config files rmsprop
+echo "Running evolution experiment with config5.txt"
+
+for ((i=1; i<=5; i++)); do
+    exp_path="multi_100_exp_rms_$i"
+    
+    python run_evolution.py \
+        --experiment_path "$exp_path" \
+        --config_file 'config_files/config5.txt' \
+        --data_path cifar10_data \
+        --log_level INFO
+done
