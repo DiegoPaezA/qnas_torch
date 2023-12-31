bc6dda47b4333727f44a97c294b4c78a890146e9
diegopaez <diripar8@gmail.com>
2023-12-31 01:37:56 -0300

    remove self variables

--- cnn/input.py
+++ cnn/input.py
@@ -9,11 +9,10 @@ import random
 import util
 import os
 from time import time
-import numpy as np
-from collections import defaultdict
 import torchvision.datasets
 from torch.utils.data import DataLoader, Subset
 from torchvision.transforms import ToTensor, Resize, Compose, RandomCrop, Normalize, TrivialAugmentWide
+from sklearn.model_selection import StratifiedShuffleSplit
 
 cifar10_info = {
   'dataset': 'CIFAR10',
@@ -62,98 +61,78 @@ class GenericDataLoader:
     self.info_dict = {'dataset': f'{self.params["dataset"]}'}
     self.info_dict['seed'] = seed
     self.download_status = not os.path.exists(self.params['data_path'])
+    
     if info is None:
         # Check if the dataset is available in the available_datasets dict
         if self.params['dataset'].lower() in available_datasets.keys():
-          dataset_info = getattr(self, f"{self.params['dataset'].lower()}_info", None)
-          self.mean = dataset_info['mean']
-          self.std = dataset_info['std']
-          self.channels, self.height, self.width = dataset_info['shape']
+          dataset_info = available_datasets[self.params['dataset'].lower()]
+          mean = dataset_info['mean']
+          std = dataset_info['std']
+          channels, height, width = dataset_info['shape']
           self.num_classes = dataset_info['num_classes']
+          
         # check if the dataset is in the torchvision datasets and compute the parameters
         elif hasattr(torchvision.datasets, self.params['dataset'].upper()):
           dataset_class = getattr(torchvision.datasets, self.params['dataset'].upper())
           dataset_ = dataset_class(self.params['data_path'], download=True, transform=ToTensor())
           loader = DataLoader(dataset_, batch_size=len(dataset_), num_workers=0, shuffle=False)
           data = next(iter(loader))
-          self.mean = data[0].mean(dim=(0, 2, 3)).tolist()
-          self.std = data[0].std(dim=(0, 2, 3)).tolist()
-          self.channels, self.height, self.width = dataset_[0][0].shape
+          mean = data[0].mean(dim=(0, 2, 3)).tolist()
+          std = data[0].std(dim=(0, 2, 3)).tolist()
+          channels, height, width = dataset_[0][0].shape
           self.num_classes = len(dataset_.classes)
         else:
           raise ValueError(f"Dataset class {self.params['dataset']} not found in torchvision.datasets or available_datasets.")
     else:
       raise NotImplementedError('Custom dataset is not implemented yet.')             
-    self.info_dict['shape'] = [self.channels, self.height, self.width]  
-    self.info_dict['mean'] = self.mean
-    self.info_dict['std'] = self.std
+    
+    self.info_dict['shape'] = [channels, height, width]  
+    self.info_dict['mean'] = mean
+    self.info_dict['std'] = std
+    
     # Transformations
-    transform = Compose([
+    self.transform = Compose([
         ToTensor(),
-        Normalize(mean=self.mean, std=self.std)
+        Normalize(mean=mean, std=std)
     ])
-    if self.params['data_aug']:
+    
+    if self.params['data_augmentation']:
         pad = 4
-        train_transform = Compose([
-            Resize((self.height + pad, self.width + pad)),
-            RandomCrop((self.height, self.width)),
+        self.train_transform = Compose([
+            Resize((height + pad, width + pad)),
+            RandomCrop((height, width)),
             TrivialAugmentWide(num_magnitude_bins=31),
             ToTensor(),
-            Normalize(mean=self.mean, std=self.std)
+            Normalize(mean=mean, std=std)
         ])
     else:
-        train_transform = Compose([
+        self.train_transform = Compose([
             ToTensor(),
-            Normalize(mean=self.mean, std=self.std)
-        ])     
-    # create the dataset
-    if hasattr(torchvision.datasets, self.params['dataset'].upper()):
-      dataset_class = getattr(torchvision.datasets, self.params['dataset'].upper())
-      self.train_dataset = dataset_class(self.params['data_path'], train=True, download=self.download_status,transform=train_transform)
-      self.valid_dataset = dataset_class(self.params['data_path'], train=True, download=self.download_status,transform=transform)
-      self.test_dataset = dataset_class(self.params['data_path'], train=False, download=self.download_status,transform=transform)
-    else:
-      raise NotImplementedError('Custom dataset is not implemented yet.')
-        
-  # Function to balance the dataset by class
-  def _balance_dataset(self,dataset, indices, max_samples_per_class):
-    """
-      Balance the dataset by class.
-      
-      Parameters:
-          dataset: The dataset object.
-          indices (list): List of indices.
-          max_samples_per_class (int): Maximum samples allowed per class.
-      
-      Returns:
-          list: Balanced indices.
-    """
-    class_samples = defaultdict(int)
-    balanced_indices = []
-    random.shuffle(indices)
-    for idx in indices:
-        _, target = dataset[idx]
-        if class_samples[target] < max_samples_per_class:
-            balanced_indices.append(idx)
-            class_samples[target] += 1
-            if len(balanced_indices) == max_samples_per_class * len(set(class_samples.values())):
-                break
-    return balanced_indices
+            Normalize(mean=mean, std=std)
+        ])
       
   def get_loader(self, for_train=True):
     """
     Get data loader for training or validation/testing.
 
     Parameters:
-      for_train (bool): If True, returns the training loader; otherwise, returns the validation/testing loader.
+      for_train (bool): If True, returns the training and val loader; otherwise, returns the testing loader.
 
     Returns:
       DataLoader: PyTorch DataLoader.
     """
+    # create the dataset
+    if hasattr(torchvision.datasets, self.params['dataset'].upper()):
+      dataset_class = getattr(torchvision.datasets, self.params['dataset'].upper())
+      full_dataset = dataset_class(self.params['data_path'], train=True, download=self.download_status)
+      test_dataset = dataset_class(self.params['data_path'], train=False, download=self.download_status,transform=self.transform)
+      self.download_status = not os.path.exists(self.params['data_path'])
+    else:
+      raise NotImplementedError('Custom dataset is not implemented yet.')
         
     if not for_train:
       test_loader = DataLoader(
-        self.test_dataset,
+        test_dataset,
         batch_size=self.params['eval_batch_size'],
         num_workers=0,
         shuffle=False,
@@ -161,44 +140,60 @@ class GenericDataLoader:
       return test_loader
     
     val_split = 1 - self.train_split
-    num_train = len(self.train_dataset)
-    indices = list(range(num_train))
-    split = int(np.floor(val_split * num_train)) + 1
+    # Get the labels and create StratifiedShuffleSplit
+    labels = full_dataset.targets
+    stratified_split = StratifiedShuffleSplit(n_splits=1, test_size=val_split)
+
+    # Get the training and validation indices
+    train_idx, val_idx = next(stratified_split.split(labels, labels))
     
-    np.random.shuffle(indices)
-    train_indices, val_indices = indices[split:], indices[:split]
+    num_train = len(full_dataset)
     
+    # split the dataset into train and validation
     if self.params['limit_data'] and self.params['limit_data_value'] < num_train:
-      train_samples = int(self.train_split * self.params['limit_data_value'])
-      val_samples = int(self.params['limit_data_value'] - train_samples)
+      train_samples = (int(self.train_split * self.params['limit_data_value'])) 
+      val_samples = (int(self.params['limit_data_value'] - train_samples))
       
-      max_samples_per_class_train = train_samples/self.num_classes
-      max_samples_per_class_val = val_samples/self.num_classes
-      train_indices_limited = self._balance_dataset(self.train_dataset, train_indices, max_samples_per_class_train)
-      val_indices_limited = self._balance_dataset(self.valid_dataset, val_indices, max_samples_per_class_val)
-      self.train_dataset = Subset(self.train_dataset, train_indices_limited)
-      self.valid_dataset = Subset(self.valid_dataset, val_indices_limited)
+      train_count_per_class = train_samples // self.num_classes
+      val_count_per_class = val_samples // self.num_classes
+      
+      train_indices = []
+      val_indices = []
+      for label in set(labels):
+        label_indices = [i for i in train_idx if labels[i] == label]
+        train_indices.extend(label_indices[:train_count_per_class])
+
+        label_indices = [i for i in val_idx if labels[i] == label]
+        val_indices.extend(label_indices[:val_count_per_class])
+      
+      train_dataset = Subset(full_dataset, train_indices)
+      valid_dataset = Subset(full_dataset, val_indices)
+
     else:
-      self.train_dataset = Subset(self.train_dataset, train_indices)
-      self.valid_dataset = Subset(self.valid_dataset, val_indices)
-      
+      train_dataset = Subset(full_dataset, train_idx)
+      valid_dataset = Subset(full_dataset, val_idx)
+    
+    # Apply transformations to the datasets
+    train_dataset.dataset.transform = self.train_transform
+    valid_dataset.dataset.transform = self.transform
+    
     train_loader = DataLoader(
-      self.train_dataset,
+      train_dataset,
       batch_size=self.params['batch_size'],
       num_workers=0,
       shuffle=True,
       pin_memory=True)
 
     val_loader = DataLoader(
-      self.valid_dataset,
+      valid_dataset,
       batch_size=self.params['eval_batch_size'],
       num_workers=0,
       shuffle=False,
       pin_memory=True)
   
-    self.info_dict['train_records'] = len(self.train_dataset)
-    self.info_dict['valid_records'] = len(self.valid_dataset)
-    self.info_dict['test_records'] = len(self.test_dataset)
+    self.info_dict['train_records'] = len(train_dataset)
+    self.info_dict['valid_records'] = len(valid_dataset)
+    self.info_dict['test_records'] = len(test_dataset)
             
     util.create_info_file(out_path=self.params['data_path'], info_dict=self.info_dict)
     
