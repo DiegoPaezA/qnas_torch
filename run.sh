7e293942df2e2502e9e1162506f3454e1c734f3f
DiegoPaez <diripar8@gmail.com>
2024-01-02 21:04:16 +0000

    testing config

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Run the evolution experiment
-echo "Running evolution experiment"
+echo "Running evolution experiment with config2.txt"
 
-python run_evolution.py \
-    --experiment_path my_exp_4 \
-    --config_file 'config_files/config2.txt' \
-    --data_path cifar10_data \
-    --log_level INFO
-
-python run_evolution.py \
-    --experiment_path my_exp_5 \
-    --config_file 'config_files/config3.txt' \
-    --data_path cifar10_data \
-    --log_level INFO
+for ((i=1; i<=5; i++)); do
+    exp_path="new_50_exp_adamw_$i"
+    
+    python run_evolution.py \
+        --experiment_path "$exp_path" \
+        --config_file 'config_files/config2.txt' \
+        --data_path cifar10_data \
+        --log_level INFO
+done
