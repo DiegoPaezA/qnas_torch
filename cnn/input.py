72350f5417de32613c0b8e0d33538d7bef29f43a
DiegoPaez <diripar8@gmail.com>
2024-01-02 16:29:49 -0300

    move img to device

--- cnn/input.py
+++ cnn/input.py
@@ -10,8 +10,8 @@ import util
 import os
 from time import time
 import torchvision.datasets
-from torch.utils.data import DataLoader, Subset, Dataset, TensorDataset
-from torchvision.transforms import ToTensor, Resize, Compose, RandomCrop, Normalize, TrivialAugmentWide
+from torch.utils.data import DataLoader, Subset, Dataset
+from torchvision.transforms import ToTensor, Resize, Compose, RandomCrop, Normalize, TrivialAugmentWide, ToPILImage
 from sklearn.model_selection import StratifiedShuffleSplit
 
 cifar10_info = {
@@ -36,14 +36,16 @@ available_datasets = {
 }
 
 class MyDataset(Dataset):
-    def __init__(self, subset, transform=None):
+    def __init__(self, subset, transform=None, device='cpu'):
         self.subset = subset
         self.transform = transform
+        self.device = device
         
     def __getitem__(self, index):
         x, y = self.subset[index]
         if self.transform:
-            x = self.transform(x)
+          x = self.transform(x)
+          x = x.to(self.device)
         return x, y
         
     def __len__(self):
@@ -125,7 +127,7 @@ class GenericDataLoader:
             Normalize(mean=mean, std=std)
         ])
       
-  def get_loader(self, for_train=True):
+  def get_loader(self, for_train=True, device='cpu'):
     """
     Get data loader for training or validation/testing.
 
@@ -188,22 +190,22 @@ class GenericDataLoader:
       valid_subset = Subset(full_dataset, val_idx)
     
     # Apply transformations to the datasets
-    train_dataset = MyDataset(train_subset, transform=self.train_transform)
-    valid_dataset = MyDataset(valid_subset, transform=self.transform)
+    train_dataset = MyDataset(train_subset, transform=self.train_transform, device=device)
+    valid_dataset = MyDataset(valid_subset, transform=self.transform, device=device)
     
     train_loader = DataLoader(
       train_dataset,
       batch_size=self.params['batch_size'],
       num_workers=0,
       shuffle=True,
-      pin_memory=True)
+      pin_memory=False)
 
     val_loader = DataLoader(
       valid_dataset,
       batch_size=self.params['eval_batch_size'],
       num_workers=0,
       shuffle=False,
-      pin_memory=True)
+      pin_memory=False)
   
     self.info_dict['train_records'] = len(train_dataset)
     self.info_dict['valid_records'] = len(valid_dataset)
