83afe924b67f5f49b1656b16974734fa6c390265
DiegoPaez <diripar8@gmail.com>
2024-01-09 20:37:38 +0000

    done: change examples

--- run_multi.sh
+++ run_multi.sh
@@ -4,7 +4,7 @@
 echo "Running evolution experiment with config2.txt"
 
 for ((i=1; i<=5; i++)); do
-    exp_path="multi_50_exp_adamw_$i"
+    exp_path="exp3_adamw_repeat_$i"
     
     python run_evolution.py \
         --experiment_path "$exp_path" \
@@ -13,41 +13,15 @@ for ((i=1; i<=5; i++)); do
         --log_level INFO
 done
 
-# Run the evolution experiment multiple times with different config files
-echo "Running evolution experiment with config3.txt"
-
-for ((i=1; i<=5; i++)); do
-    exp_path="multi_100_exp_adamw_$i"
-    
-    python run_evolution.py \
-        --experiment_path "$exp_path" \
-        --config_file 'config_files/config3.txt' \
-        --data_path cifar10_data \
-        --log_level INFO
-done
-
 # Run the evolution experiment multiple times with different config files rmsprop
 echo "Running evolution experiment with config4.txt"
 
 for ((i=1; i<=5; i++)); do
-    exp_path="multi_50_exp_rms_$i"
+    exp_path="exp3_rms_repeat$i"
     
     python run_evolution.py \
         --experiment_path "$exp_path" \
         --config_file 'config_files/config4.txt' \
-        --data_path cifar10_data \
-        --log_level INFO
-done
-
-# Run the evolution experiment multiple times with different config files rmsprop
-echo "Running evolution experiment with config5.txt"
-
-for ((i=1; i<=5; i++)); do
-    exp_path="multi_100_exp_rms_$i"
-    
-    python run_evolution.py \
-        --experiment_path "$exp_path" \
-        --config_file 'config_files/config5.txt' \
         --data_path cifar10_data \
         --log_level INFO
 done
