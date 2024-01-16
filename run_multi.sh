7495c417fd3a5786516a878b4c7c3b75b980d8d1
DiegoPaez <diripar8@gmail.com>
2024-01-16 19:01:14 +0000

    working: experiment 7 depth wise separable convolution

--- run_multi.sh
+++ run_multi.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Run the evolution experiment multiple times
-echo "Running evolution experiment with config2.txt"
+echo "Running evolution experiment with config6.txt"
 
 for ((i=1; i<=5; i++)); do
-    exp_path="exp5_adamw_repeat_$i"
+    exp_path="exp7_adamw_repeat_$i"
     
     python run_evolution.py \
         --experiment_path "$exp_path" \
-        --config_file 'config_files/config2.txt' \
+        --config_file 'config_files/config6.txt' \
         --data_path cifar10_data \
         --log_level INFO
 done
 
 # Run the evolution experiment multiple times with different config files rmsprop
-echo "Running evolution experiment with config4.txt"
+echo "Running evolution experiment with config7.txt"
 
 for ((i=1; i<=5; i++)); do
-    exp_path="exp5_rms_repeat$i"
+    exp_path="exp7_rms_repeat$i"
     
     python run_evolution.py \
         --experiment_path "$exp_path" \
-        --config_file 'config_files/config4.txt' \
+        --config_file 'config_files/config7.txt' \
         --data_path cifar10_data \
         --log_level INFO
 done
