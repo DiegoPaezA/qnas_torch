82f9ac2643f03523ece82185199bd8aaac0a94e7
DiegoPaez <diripar8@gmail.com>
2024-01-25 13:20:53 +0000

    testing new configs

--- run_multi.sh
+++ run_multi.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Run the evolution experiment multiple times
-echo "Running evolution experiment with config6.txt"
+echo "Running evolution experiment with config2.txt"
 
 for ((i=1; i<=5; i++)); do
-    exp_path="exp8_adamw_repeat_$i"
+    exp_path="exp9_adamw_repeat_$i"
     
     python run_evolution.py \
         --experiment_path "$exp_path" \
-        --config_file 'config_files/config6.txt' \
+        --config_file 'config_files/config2.txt' \
         --data_path cifar10_data \
         --log_level INFO
 done
 
 # Run the evolution experiment multiple times with different config files rmsprop
-echo "Running evolution experiment with config7.txt"
+# echo "Running evolution experiment with config7.txt"
 
-for ((i=1; i<=5; i++)); do
-    exp_path="exp8_rms_repeat$i"
+# for ((i=1; i<=5; i++)); do
+#     exp_path="exp9_rms_repeat$i"
     
-    python run_evolution.py \
-        --experiment_path "$exp_path" \
-        --config_file 'config_files/config7.txt' \
-        --data_path cifar10_data \
-        --log_level INFO
-done
+#     python run_evolution.py \
+#         --experiment_path "$exp_path" \
+#         --config_file 'config_files/config7.txt' \
+#         --data_path cifar10_data \
+#         --log_level INFO
+# done
