f599d01258a9e0ad77922cf659a8fa85905e2a7e
DiegoPaez <diripar8@gmail.com>
2024-02-12 14:26:45 +0000

    Done: update scripts

---
+++ run_one_retrain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+exp_path="retrain_experiments/exp13_adamw_repeat_1"
+
+python run_retrain.py \
+    --experiment_path "$exp_path" \
+    --data_path cifar10_data \
+    --retrain_folder retrain_1 \
+    --log_level INFO \
+    --max_epochs 300 \
+    --batch_size 256 \
+    --eval_batch_size 1000 \
+    --device cuda:0 \
+    --num_repetitions 1
