af0b869547c3a2f0ab07dd7825a622b8bd473316
DiegoPaez <diripar8@gmail.com>
2024-02-13 19:13:38 +0000

    Done: set availables gpus using CUDA_VISIBLE_DEVICES=0,1

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Run the evolution experiment multiple times
-echo "Running evolution experiment with config12.txt"
+echo "Running evolution experiment with config2.txt"
 
 exp_path="exp_test_x"
-    
-python run_evolution.py \
+
+# CUDA_VISIBLE_DEVICES="0,1" python run_evolution.py \    
+CUDA_VISIBLE_DEVICES=1 python run_evolution.py \
         --experiment_path "$exp_path" \
-        --config_file 'config_files/config12.txt' \
+        --config_file 'config_files/config2.txt' \
         --data_path cifar10_data \
         --log_level INFO
