5384c5ec846f9b6a3f0c24374b3e88f4b2a8be6d
DiegoPaez <diripar8@gmail.com>
2024-06-07 14:42:12 +0000

    flops counter

--- cnn/train.py
+++ cnn/train.py
@@ -22,6 +22,7 @@ from cnn import model, input
 from util import create_info_file, init_log, load_yaml
 from torch.cuda.amp import GradScaler
 from torch.profiler import profile, record_function, ProfilerActivity
+from fvcore.nn import FlopCountAnalysis
 
 
 
@@ -179,6 +180,9 @@ def train(model:torch.nn.Module, criterion:torch.nn.Module, optimizer:torch.opti
     total_params = sum(p.numel() for p in model.parameters())
     total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     
+    flops_counter = FlopCountAnalysis(model, inference_images)
+    total_flops = flops_counter.total() / len(inference_images) # Average flops per image
+    
     # Scalarized multi-objective function
     scalar_multi_objective = params['acc_weight']*best_accuracy - params['params_weight']*(total_trainable_params / MAX_PARAMS) - params['time_weight']*(cuda_inference_time / MAX_INFER_TIME)
     
@@ -187,6 +191,7 @@ def train(model:torch.nn.Module, criterion:torch.nn.Module, optimizer:torch.opti
     params['cuda_inference_time'] = cuda_inference_time
     params['cpu_inference_time'] = cpu_inference_time
     params['model_memory_usage'] = model_memory_usage
+    params['total_flops'] = total_flops
     params['best_accuracy'] = best_accuracy
     params['mean_eval_accuracy'] = mean_eval_accuracy
     params['median_eval_accuracy'] = median_eval_accuracy
@@ -205,6 +210,7 @@ def train(model:torch.nn.Module, criterion:torch.nn.Module, optimizer:torch.opti
     training_results['cuda_inference_time'] = cuda_inference_time # in microseconds
     training_results['model_memory_usage'] = model_memory_usage # in MB
     training_results['total_trainable_params'] = total_trainable_params
+    training_results['total_flops'] = total_flops
     training_results['best_accuracy'] = best_accuracy
     training_results['mean_eval_accuracy'] = mean_eval_accuracy
     training_results['median_eval_accuracy'] = median_eval_accuracy
